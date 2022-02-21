8f5425190e3d2228d5b67b014d0f7931fb31cd35
Nina Bjaerum <nina.bjaerum@skye.ey.com>
2022-02-21 22:07:14 +0100

    new test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import LongText from "./components/LongText";
+import { Typography } from "@mui/material";
+import Summary from "./components/Summary";
 
 test("renders learn react link", () => {
-  render(<LongText />);
-  const linkElement = screen.getByText(/oss/i);
+  render(<Summary />);
+  const linkElement = screen.getByText(/10/i);
   expect(linkElement).toBeInTheDocument();
 });
